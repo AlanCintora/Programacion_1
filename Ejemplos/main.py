15e4fefa629f08307e7916d6a6111dcf66be9a5f
AlanCintora <alancintora99@gmail.com>
2025-04-16 21:13:31 -0300

    se agregan condicionales en la rama nueva

--- Ejemplos/main.py
+++ Ejemplos/main.py
@@ -2,3 +2,10 @@ nombre = "Alan"
 edad = 25
 print ("Hola, soy " + nombre + " y tengo " + str(edad) + " años.")
 tieneMascota = True
+cantidadMascotas = 1
+tipoMascotas = "gato"
+
+if edad >= 18:
+    print ("Puede ingresar al sistema")
+else:
+    print("No puede ingresar al sistema")
